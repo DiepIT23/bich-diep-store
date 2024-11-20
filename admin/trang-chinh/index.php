ac09f8c8dcb5003ddd061fa36e36ec87b3d3ae92
DiepIT23 <diepttbph51197@gmail.com>
2024-11-20 22:11:31 +0700

    Diệp đẩy chức năng

--- admin/trang-chinh/index.php
+++ admin/trang-chinh/index.php
@@ -2,3 +2,27 @@
 require_once "../../global.php";
 $VIEW_NAME = "../trang-chinh/home.php";
 require_once "../layout.php";
+
+require_once 'models/Product.php';
+require_once 'admin/trang-chinh/index.php';
+
+$cProduct = new ProductController();
+$luaChon = isset($_GET['act']) ? $_GET['act'] :'/';
+switch($luaChon){
+    case 'getAllProduct':
+        $cProduct->getAllProduct();
+        break;
+     case 'addProduct':
+         $cProduct->addProduct();
+         break; 
+    case 'editProduct':
+         $cProduct->editDataProduct();
+         break;
+    // case 'deleteProduct':
+    //     $cProduct->deleteDataProduct();
+    //     break;
+    default:
+        $cProduct->getAllProduct();
+        break;
+    }
+?>
