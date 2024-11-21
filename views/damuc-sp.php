7f99732fc3208d2e57512822240cfc45685b45de
Manh Linh <mlinhalone@gmail.com>
2024-11-21 23:47:13 +0700

    Linh update giao diện web client

--- views/damuc-sp.php
+++ views/damuc-sp.php
@@ -3,15 +3,15 @@
        <h2 class="mb-4">Danh Mục Sản Phẩm</h2>
        <div class="row justify-content-center p-3">
            <div class="col-3 p-2">
-               <a href="">
+               <a href="<?= $ROOT_URL ?>/?act=ao&ao=ao-khoac-ni">
                    <div class="bg-light d-flex align-items-center justify-content-evenly border-radius-20 pro">
-                       <p class="roboto-title">Áo khoác</p>
+                       <p class="roboto-title">Áo khoác nỉ</p>
                        <img src="<?= $IMAGES_URL ?>/AKGOL418-35-510x510.png" width="120px">
                    </div>
                </a>
            </div>
            <div class="col-3 p-2">
-               <a href="">
+               <a href="<?= $ROOT_URL ?>/?act=ao&ao=ao-quan-ni">
                    <div class="bg-light d-flex align-items-center justify-content-evenly border-radius-20 pro">
                        <p class="roboto-title">Áo nỉ</p>
                        <img src="<?= $IMAGES_URL ?>/ANHTK504-1.png" width="120px">
@@ -19,7 +19,7 @@
                </a>
            </div>
            <div class="col-3 p-2">
-               <a href="">
+               <a href="<?= $ROOT_URL ?>/?act=ao&ao=ao-somi">
                    <div class="bg-light d-flex align-items-center justify-content-evenly border-radius-20 pro">
                        <p class="roboto-title">Sơ mi</p>
                        <img src="<?= $IMAGES_URL ?>/SKDTK527-1-510x510.png" width="120px">
@@ -27,7 +27,7 @@
                </a>
            </div>
            <div class="col-3 p-2">
-               <a href="">
+               <a href="<?= $ROOT_URL ?>/?act=ao&ao=ao-khoac-jeans">
                    <div class="bg-light d-flex align-items-center justify-content-evenly border-radius-20 pro">
                        <p class="roboto-title">Áo Khoác Jeans</p>
                        <img src="<?= $IMAGES_URL ?>/AKBTK502-1.png" width="120px">
@@ -43,7 +43,7 @@
                </a>
            </div>
            <div class="col-3 p-2">
-               <a href="">
+               <a href="<?= $ROOT_URL ?>/?act=quan&quan=quan-kaki">
                    <div class="bg-light d-flex align-items-center justify-content-evenly border-radius-20 pro">
                        <p class="roboto-title">Quần Kaki</p>
                        <img src="<?= $IMAGES_URL ?>/QKLTK404-3-768x768.png" width="120px">
@@ -51,7 +51,7 @@
                </a>
            </div>
            <div class="col-3 p-2">
-               <a href="">
+               <a href="<?= $ROOT_URL ?>/?act=quan&quan=quan-jeans">
                    <div class="bg-light d-flex align-items-center justify-content-evenly border-radius-20 pro">
                        <p class="roboto-title">Quần Jeans</p>
                        <img src="<?= $IMAGES_URL ?>/QJDTK506-1-768x768.png" width="120px">
@@ -59,7 +59,7 @@
                </a>
            </div>
            <div class="col-3 p-2">
-               <a href="">
+               <a href="<?= $ROOT_URL ?>/?act=quan&quan=quan-au">
                    <div class="bg-light d-flex align-items-center justify-content-evenly border-radius-20 pro">
                        <p class="roboto-title">Quần Âu</p>
                        <img src="<?= $IMAGES_URL ?>/QATTK408-1-1.png" width="120px">
