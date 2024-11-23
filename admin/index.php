e5f915717bc67bf8ef7e17990fce4d34d7af2515
DiepIT23 <diepttbph51197@gmail.com>
2024-11-23 15:27:53 +0700

    diep update code

--- admin/index.php
+++ admin/index.php
@@ -2,7 +2,7 @@
 require_once "../global.php";
 include_once "menu.php";
 include_once "../models/connectDB.php";
-include_once "../models/san-pham.php";
+include_once "../models/Sanpham.php";
 include_once "../models/Binhluan.php";
 include_once "../models/Danhmuc.php";
 
@@ -12,8 +12,12 @@ if (isset($_GET['act']) && $_GET['act'] !== "") {
 
             // ===================== CONTROLLER DANH MỤC ===================== //
 
-        case 'value':
-            # code...
+        case 'them-danhmuc':
+           if(isset($_POST["Themmoi"])){
+            $ten_loai = $_POST["ten_loai"];
+            // insertDataDanhmuc($ten_loai);
+
+           }
             break;
 
         default:
