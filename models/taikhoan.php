2b60ac53a8847ed66cea87fa922109ded89e3072
DiepIT23 <diepttbph51197@gmail.com>
2024-11-26 15:03:31 +0700

    diep up dơn hàng và bình luận

--- models/taikhoan.php
+++ models/taikhoan.php
@@ -2,54 +2,50 @@
 function loadall_taikhoan()
 {
     $sql = "SELECT * FROM tai_khoan";
-    return pdo_query($sql);
+    $tk = pdo_query($sql);
+    return $tk;
 }
 
-function loadone_taikhoan($id_user)
-{
-    $sql = "SELECT * FROM tai_khoan WHERE id_user = ?";
-    return pdo_query_one($sql, [$id_user]);
+function loadone_taikhoan($id_user){
+    $sql = "SELECT * FROM tai_khoan WHERE id_user='$id_user'";
+    $tk = pdo_query($sql);
+    return $tk;
 }
-
-function insert_taikhoan($ten_user, $email, $ten_dang_nhap, $mat_khau, $dia_chi, $sdt)
+function insert_taikhoan($ten_user, $mat_khau, $email,$sdt)
 {
-    $sql = "INSERT INTO `tai_khoan`( `ten_user`, `email`, `ten_dang_nhap`, `mat_khau`,  `dia_chi`, `sdt`) 
-    VALUES ('$ten_user','$email','$ten_dang_nhap','$mat_khau','$dia_chi','$sdt')";
+    $sql = " INSERT INTO tai_khoan(ten_user,mar_khau,email,sdt) VALUES('$ten_user','$mat_khau','$email','$sdt')";
     pdo_execute($sql);
 }
-
-function check_user($ten_dang_nhap, $mat_khau)
-{
-    $sql = "SELECT * FROM tai_khoan WHERE ten_dang_nhap = '$ten_dang_nhap' AND mat_khau ='$mat_khau'";
-    $sp = pdo_query_one($sql);
-    return $sp;
-}
-
-function checkemail($email)
-{
-    $sql = "SELECT * FROM tai_khoan WHERE email = ?";
-    return pdo_query_one($sql, [$email]);
-}
-
-function update_taikhoan($id_user, $ten_user, $email, $ten_dang_nhap, $mat_khau, $hinh_anh, $dia_chi, $sdt, $vai_tro)
-{
-    $sql = "UPDATE taikhoan SET 
-            ten_user = ?, email = ?, ten_dang_nhap = ?, mat_khau = ?, 
-            hinh_anh = ?, dia_chi = ?, sdt = ?, vai_tro = ? 
-            WHERE id_user = ?";
-    pdo_execute($sql, [$ten_user, $email, $ten_dang_nhap, $mat_khau, $hinh_anh, $dia_chi, $sdt, $vai_tro, $id_user]);
-}
-
-function update_password($id_user, $mat_khau)
-{
-    $sql = "UPDATE taikhoan SET mat_khau = ? WHERE id_user = ?";
-    pdo_execute($sql, [$mat_khau, $id_user]);
-}
-
-function delete_taikhoan($id_user)
-{
-    $sql = "DELETE FROM tai_khoan WHERE id_user = ?";
-    pdo_execute($sql, [$id_user]);
-}
-?>
-ư
+// function check_user($user, $pass)
+// {
+//     $sql = "SELECT * FROM taikhoan WHERE user='$user' AND pass ='$pass'";
+//     $sp = pdo_query_one($sql);
+//     return $sp;
+// }
+// function checkemail($email)
+// {
+//     $sql = "SELECT * FROM taikhoan WHERE email='$email'";
+//     $sp = pdo_query_one($sql);
+//     return $sp;
+// }
+// function update_taikhoan($id,$user,$pass,$email, $address, $tel)
+// {
+//     $sql = " UPDATE taikhoan SET user='$user',pass='$pass', email='$email' , address='$address' , tel='$tel' WHERE id = '$id' ";
+//     pdo_execute($sql);
+// }
+// function update_mk($id,$user,$newpass)
+// {
+//     $sql = " UPDATE taikhoan SET user='$user',pass='$newpass' WHERE id = '$id'";
+//     pdo_execute($sql);
+// }
+// function loadone_tk($id){
+//     $sql = "SELECT * FROM taikhoan WHERE id='$id'";
+//     $taikhoan = pdo_query_one($sql);
+//     return $taikhoan;
+// }
+// function delete_tk($id){
+//     $sql = "DELETE FROM taikhoan WHERE id='$id'";
+//     $taikhoan = pdo_query_one($sql);
+//     return $taikhoan;
+// }
+// ?>
