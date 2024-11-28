a9189870415a388b85fffb3d696c6d069dcbd418
Manh Linh <mlinhalone@gmail.com>
2024-11-28 14:56:06 +0700

    update

--- views/sp-banchay.php
+++ views/sp-banchay.php
@@ -6,37 +6,37 @@
             extract($sanpham);
             $gia_ban = $don_gia - ($don_gia * $giam_gia / 100);
         ?>
-        <div class="col-lg-2">
-            <div class="card product-card">
-                <img src="<?= $IMAGES_URL . "/" . $hinh_anh ?>" class="card-img-top" alt="<?= $ten_sp ?>"
-                    height="300px">
-                <div class="card-body">
-                    <p class="card-title"
-                        style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis; max-width: 20ch;">
-                        <?= $ten_sp ?>
-                    </p>
-                    <p class="roboto-small"><?= number_format($gia_ban, 0, ',', '.') ?> VND</p>
-                </div>
-                <div class="card-overlay">
-                    <!-- Nút Xem chi tiết -->
-                    <a href="<?= $ROOT_URL ?>/?act=sp-chitiet&id_sp=<?= $id_sp ?>" class="btn btn-light mb-2">Xem chi
-                        tiết</a>
+            <div class="col-lg-2">
+                <div class="card product-card">
+                    <img src="<?= $IMAGES_URL . "/" . $hinh_anh ?>" class="card-img-top" alt="<?= $ten_sp ?>"
+                        height="300px">
+                    <div class="card-body">
+                        <p class="card-title"
+                            style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis; max-width: 20ch;">
+                            <?= $ten_sp ?>
+                        </p>
+                        <p class="roboto-small"><?= number_format($gia_ban, 0, ',', '.') ?> VND</p>
+                    </div>
+                    <div class="card-overlay">
+                        <!-- Nút Xem chi tiết -->
+                        <a href="<?= $ROOT_URL ?>/?act=sp-chitiet&id_sp=<?= $id_sp ?>" class="btn btn-light mb-2">Xem chi
+                            tiết</a>
 
-                    <!-- Nút Thêm vào giỏ -->
-                    <form method="post" action="<?= $ROOT_URL ?>/?act=add_to_cart" class="d-inline">
-                        <input type="hidden" name="id_sp" value="<?= $id_sp ?>">
-                        <input type="hidden" name="ten_sp" value="<?= $ten_sp ?>">
-                        <input type="hidden" name="don_gia" value="<?= $don_gia ?>">
-                        <input type="hidden" name="hinh_anh" value="<?= $hinh_anh ?>">
-                        <button type="submit" class="btn btn-light mb-2">Thêm vào giỏ</button>
-                    </form>
+                        <!-- Nút Thêm vào giỏ -->
+                        <form method="post" action="<?= $ROOT_URL ?>/?act=add_to_cart" class="d-inline">
+                            <input type="hidden" name="id_sp" value="<?= $id_sp ?>">
+                            <input type="hidden" name="ten_sp" value="<?= $ten_sp ?>">
+                            <input type="hidden" name="don_gia" value="<?= $gia_ban ?>">
+                            <input type="hidden" name="hinh_anh" value="<?= $hinh_anh ?>">
+                            <button type="submit" class="btn btn-light mb-2">Thêm vào giỏ</button>
+                        </form>
 
-                    <!-- Nút Mua hàng -->
-                    <a href="<?= $ROOT_URL ?>/?act=thanhtoan&id_sp=<?= $id_sp ?>&ten_sp=<?= $ten_sp ?>&don_gia=<?= $don_gia ?>&hinh_anh=<?= $hinh_anh ?>"
-                        class="btn btn-dark">Mua hàng</a>
+                        <!-- Nút Mua hàng -->
+                        <a href="<?= $ROOT_URL ?>/?act=thanhtoan&id_sp=<?= $id_sp ?>&ten_sp=<?= $ten_sp ?>&don_gia=<?= $gia_ban ?>&hinh_anh=<?= $hinh_anh ?>"
+                            class="btn btn-dark">Mua hàng</a>
+                    </div>
                 </div>
             </div>
-        </div>
         <?php
         }
         ?>
