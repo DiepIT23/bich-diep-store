24af39dd0e79e3cbdf117f959cb7168d592436f9
Manh Linh <mlinhalone@gmail.com>
2024-11-30 15:13:32 +0700

    update

--- models/taikhoan.php
+++ models/taikhoan.php
@@ -1,12 +1,12 @@
 <?php
 function loadall_taikhoan()
 {
-  $sql = "SELECT * FROM tai_khoan";
-  $listtaikhoan = pdo_query($sql);
-  return $listtaikhoan;
+    $sql = "SELECT * FROM tai_khoan";
+    $listtaikhoan = pdo_query($sql);
+    return $listtaikhoan;
 }
 
-function insert_taikhoan($ten_user,$mat_khau, $email, $ten_dang_nhap,  $dia_chi, $sdt)
+function insert_taikhoan($ten_user, $mat_khau, $email, $ten_dang_nhap,  $dia_chi, $sdt)
 
 {
     $sql = " INSERT INTO tai_khoan(ten_user,mat_khau,email,ten_dang_nhap,dia_chi,sdt) VALUES('$ten_user','$mat_khau','$email','$ten_dang_nhap','$dia_chi','$sdt')";
@@ -30,12 +30,10 @@ function checkemail($email)
 
 function update_taikhoan($id_user, $ten_user, $email, $ten_dang_nhap, $mat_khau, $hinh_anh, $dia_chi, $sdt)
 {
-    if ($hinh_anh !="") {
-        $sql = " UPDATE tai_khoan SET ten_user='$ten_user',email='$email',ten_dang_nhap='$ten_dang_nhap',mat_khau='$mat_khau',hinh_anh='$hinh_anh' , dia_chi='$dia_chi',sdt='$sdt' WHERE id_user = ".$id_user;
+    if ($hinh_anh != "") {
+        $sql = " UPDATE tai_khoan SET ten_user='$ten_user',email='$email',ten_dang_nhap='$ten_dang_nhap',mat_khau='$mat_khau',hinh_anh='$hinh_anh' , dia_chi='$dia_chi',sdt='$sdt' WHERE id_user = " . $id_user;
     } else {
-        $sql = " UPDATE tai_khoan SET ten_user='$ten_user',email='$email',ten_dang_nhap='$ten_dang_nhap',mat_khau='$mat_khau',dia_chi='$dia_chi',sdt='$sdt' WHERE id_user = ".$id_user;
-    } 
+        $sql = " UPDATE tai_khoan SET ten_user='$ten_user',email='$email',ten_dang_nhap='$ten_dang_nhap',mat_khau='$mat_khau',dia_chi='$dia_chi',sdt='$sdt' WHERE id_user = " . $id_user;
+    }
     pdo_execute($sql);
 }
-?>
-
