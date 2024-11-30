b463f8a7c739bbba5ec0d10191d9b6adbd72cdbf
Manh Linh <mlinhalone@gmail.com>
2024-11-30 16:25:00 +0700

    fix tài khoản

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
+session_start();
+ob_start();
 require_once "global.php";
 include_once "models/pdo.php";
 include_once "models/Sanpham.php";
-
 include_once "models/Danhmuc.php";
-include_once "models/taikhoan.php";
+include_once "models/Taikhoan.php";
 
 if (!isset($_SESSION['mycart'])) $_SESSION['mycart'] = [];
 // Hiển thị danh mục 
@@ -102,7 +103,6 @@ if (isset($_GET['act']) && $_GET['act'] !== '') {
                     'hinh_anh' => $hinh_anh,
                     'so_luong' => $so_luong, // Sử dụng số lượng người dùng chọn
                 ];
-
                 // Kiểm tra nếu sản phẩm đã có trong giỏ, tăng số lượng
                 $exists = false;
                 foreach ($_SESSION['mycart'] as &$item) {
@@ -112,12 +112,10 @@ if (isset($_GET['act']) && $_GET['act'] !== '') {
                         break;
                     }
                 }
-
                 // Nếu chưa có trong giỏ, thêm mới
                 if (!$exists) {
                     $_SESSION['mycart'][] = $new_item;
                 }
-
                 // Chuyển hướng trở lại giỏ hàng
                 header("Location: $ROOT_URL/?act=giohang");
                 exit;
@@ -126,41 +124,73 @@ if (isset($_GET['act']) && $_GET['act'] !== '') {
             // ======================= CONTROLLER TÀI KHOẢN ======================= //
 
         case 'dangnhap':
+            if (isset($_POST["dangnhap"])) {
+                $ten_dang_nhap = $_POST["ten_dang_nhap"];
+                $mat_khau = $_POST["mat_khau"];
+                $checkuser = check_user($ten_dang_nhap, $mat_khau);
+                if (is_array($checkuser)) {
+                    $_SESSION["ten_dang_nhap"] = $checkuser;
+                    print_r($checkuser);
+                    header("location:$ROOT_URL/");
+                    exit;
+                } else {
+                    $thongbao = "Tài khoản không tồn tại , vui lòng kiểm tra hoặc đăng ký !";
+                }
+            }
             include_once "views/taikhoan/dang-nhap.php";
             break;
         case 'dangky':
+            // print_r(($_POST));
+            if (isset($_POST["dangky"])) {
+                $ten_user = $_POST["ten_user"];
+                $email = $_POST["email"];
+                $ten_dang_nhap = $_POST["ten_dang_nhap"];
+                $mat_khau = $_POST["mat_khau"];
+                // $hinh_anh = $_POST["hinh_anh"];
+                $dia_chi = $_POST["dia_chi"];
+                $sdt = $_POST["sdt"];
+                insert_taikhoan($ten_user, $email, $ten_dang_nhap, $mat_khau, $dia_chi, $sdt);
+                $thongbao = "Đăng ký thành công . Vui lòng đăng nhập để bình luận hoặc mua hàng";
+                header("Location: $ROOT_URL/?dangnhap");
+            }
             include_once "views/taikhoan/dang-ky.php";
             break;
-        // case 'edit-tk':
-        //     if (isset($_POST["capnhat"])) {
-        //         $ten_user = $_POST["ten_user"];
-        //         $email = $_POST["email"];
-        //         $ten_dang_nhap = $_POST["ten_dang_nhap"];
-        //         $mat_khau = $_POST["mat_khau"];
-        //         $hinh_anh = $_POST["hinh_anh"];
-        //         $dia_chi = $_POST["dia_chi"];
-        //         $sdt = $_POST["sdt"];
+        case "thoat":
+            if (isset($_SESSION['ten_dang_nhap'])) {
+                unset($_SESSION['ten_dang_nhap']);
+                header("location:$ROOT_URL/");
+            }
+            break;
+        case 'edit-tk':
+            if (isset($_POST["capnhat"])) {
+                $ten_user = $_POST["ten_user"];
+                $email = $_POST["email"];
+                $ten_dang_nhap = $_POST["ten_dang_nhap"];
+                $mat_khau = $_POST["mat_khau"];
+                $hinh_anh = $_POST["hinh_anh"];
+                $dia_chi = $_POST["dia_chi"];
+                $sdt = $_POST["sdt"];
 
-        //         update_taikhoan($id_user, $ten_user, $email, $ten_dang_nhap, $mat_khau, $hinh_anh, $dia_chi, $sdt);
-        //         $_SESSION["ten_dang_nhap"] =  check_user($ten_dang_nhap, $mat_khau);
-        //         header("location:$ROOT_URL/");
-        //         exit;
-        //     }
-        //     include_once "views/taikhoan/edit-tk.php";
-        //     break;
-        // case 'quenmk':
-        //     if (isset($_POST["guimk"])) {
-        //         $email = $_POST["email"];
-        //         $checkemail = checkemail($email);
-        //         if (is_array($checkemail)) {
-        //             $thongbao = "Mật khẩu của bạn là : " . $checkemail["mat_khau"];
-        //         } else {
-        //             $thongbao = "Email này không tồn tại";
-        //         }
-        //     }
-        //     include_once "views/taikhoan/quenmk.php";
-        //     break;
-       
+                update_taikhoan($id_user, $ten_user, $email, $ten_dang_nhap, $mat_khau, $hinh_anh, $dia_chi, $sdt);
+                $_SESSION["ten_dang_nhap"] =  check_user($ten_dang_nhap, $mat_khau);
+                header("location:$ROOT_URL/");
+                exit;
+            }
+            include_once "views/taikhoan/edit-tk.php";
+            break;
+        case 'quenmk':
+            if (isset($_POST["guimk"])) {
+                $email = $_POST["email"];
+                $checkemail = checkemail($email);
+                if (is_array($checkemail)) {
+                    $thongbao = "Mật khẩu của bạn là : " . $checkemail["mat_khau"];
+                } else {
+                    $thongbao = "Email này không tồn tại";
+                }
+            }
+            include_once "views/taikhoan/quenmk.php";
+            break;
+
 
 
 
@@ -178,3 +208,4 @@ if (isset($_GET['act']) && $_GET['act'] !== '') {
     include_once "views/trangchu.php";
 }
 include_once "views/footer.php";
+ob_end_flush();
